601dd6a727d1d4a846d5af65831e943b5bb4a39a
satiya3577@gmail.com <satiya3577@gmail.com>
2020-12-08 14:05:52 +0700

    Update Function Edit Personal

---
+++ app/Http/Controllers/PersonalController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Auth;
+
+class PersonalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $userID = Auth::user()->id;
+        DB::table('personals')->where('user_id', $userID)->update(
+            [
+                'address' => $request->get('address'),
+                'tel' => $request->get('tel'),
+                'person_name' => $request->get('person_name'),
+                'person_tel' => $request->get('person_tel'),
+                'person_address' => $request->get('person_address')
+            ]
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
