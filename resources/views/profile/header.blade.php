1e589a72d6ad601973e47c6a074161c612f7fac2
lamsri1992 <satiya3577@gmail.com>
2021-05-11 10:50:05 +0700

    Update Profile UI

--- resources/views/profile/header.blade.php
+++ resources/views/profile/header.blade.php
@@ -1,10 +1,11 @@
-<div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center">
-    <span class="mask bg-gradient-primary"></span>
+<div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style="background-image: url(https://new.watchanhospital.com/assets/img/hospital_front.jpg); 
+    background-size: cover; background-position: bottom;">
+    <span class="mask bg-gradient-default opacity-8"></span>
     <div class="container-fluid d-flex align-items-center">
         <div class="row">
             <div class="col-md-12 {{ $class ?? '' }}">
                 <h1 class="display-2 text-white">{{ $title }}</h1>
-                @if (isset($description) && $description)
+                @if(isset($description) && $description)
                     <p class="text-white mt-0 mb-5">{{ $description }}</p>
                 @endif
             </div>
