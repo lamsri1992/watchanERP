1a3d58eafc0b5cd16e2cdb44b04c3b39fbf686c6
lamsri1992 <satiya3577@gmail.com>
2021-12-13 18:34:05 +0700

    Update Financial Dashboard

--- resources/views/finance/index.blade.php
+++ resources/views/finance/index.blade.php
@@ -16,80 +16,126 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <div class="col-md-12">
-                        <form id="frmImport" method="POST" enctype="multipart/form-data" action="{{ url('finance/import') }}">
-                            {{ csrf_field() }}
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h1><i class="fa fa-cloud-upload-alt"></i> อัพโหลดข้อมูลเงินเดือน</h1>
-                                </div>
-                                <div class="col-md-12" style="margin-top: 0.5rem;">
-                                    <div class="custom-file">
-                                        <input id="select-file" name="select-file" type="file" class="custom-file-input" required>
-                                        <label class="custom-file-label" for="select-file">เลือกไฟล์ที่จะอัพโหลด </label>
-                                        <small class="text-danger">(ไฟล์นามสกุล .xlsx หรือ .xls เท่านั้น สามารถดูได้จากคู่มือการอัพโหลด)</small>
+                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link active" id="pills-salary-tab" data-toggle="pill" 
+                            href="#pills-salary" role="tab" aria-controls="pills-salary" aria-selected="true">
+                            ข้อมูลเงินเดือนเจ้าหน้าที่
+                            </a>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <a class="nav-link" id="pills-profile-tab" data-toggle="pill" 
+                            href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">
+                            ข้อมูลค่าล่วงเวลาเจ้าหน้าที่
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-salary" role="tabpanel" aria-labelledby="pills-salary-tab">
+                            <div class="col-md-12 jumbotron">
+                                <div class="row" style="margin-bottom: 1rem;">
+                                    <div class="col-md-6">
+                                        <h1>
+                                            <i class="fa fa-money-check-alt"></i> ข้อมูลเงินเดือน
+                                            <small class="text-muted">พนักงานกระทรวง , ลูกจ้างเหมาบริการ</small>
+                                        </h1>
+                                    </div>
+                                    <div class="col-md-6 text-right">
+                                        <button class="btn btn-success" data-toggle="modal" data-target="#import">
+                                            <i class="fa fa-cloud-upload-alt"></i> นำเข้าข้อมูลเงินเดือน
+                                        </button>
                                     </div>
                                 </div>
-                                <div class="col-md-12" style="margin-top: 0.5rem;">
-                                    <button type="submit" class="btn btn-success"><i class="fa fa-check-circle"></i> อัพโหลดไฟล์</button>
-                                    @if (count($salog) > 0)
-                                    <div class="">
-                                        <small>ประวัติการอัพโหลด</small>
-                                        <ul>
-                                            @foreach ($salog as $sals)
-                                            <li>
-                                                <small class="text-muted">{{ $sals->sal_file }} / {{ $sals->create_at }}</small>
-                                            </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                    @endif
-                                </div>
+                                <table id="datatableBasic" class="table table-borderless table-striped"
+                                    style="border-collapse: collapse; border-radius: 4px; overflow: hidden;">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th class="text-center">#</th>
+                                            <th>เจ้าหน้าที่</th>
+                                            <th>ตำแหน่ง</th>
+                                            <th>Book Bank</th>
+                                            {{-- <th class="text-right">เงินเดือน</th> --}}
+                                            <th class="text-center">ปี</th>
+                                            <th class="text-center">เดือน</th>
+                                            <th class="text-center"><i class="fa fa-bars"></i></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @php $i = 0; @endphp
+                                        @foreach ($result as $res)
+                                        @php $i++; @endphp
+                                        <tr>
+                                            <td class="text-center">{{ $i }}</td>
+                                            <td>{{ $res->name }}</td>
+                                            <td>{{ $res->position }}</td>
+                                            <td>{{ $res->acc_no }}</td>
+                                            {{-- <td class="text-right">{{ number_format($res->salary,2) }} ฿</td> --}}
+                                            <td class="text-center">{{ $res->year }}</td>
+                                            <td class="text-center">{{ MonthThai(date('Y-'.$res->month.'-d')) }}</td>
+                                            <td class="text-center">
+                                                <a href="#" class="btn btn-outline-info btn-sm btn-block">
+                                                    <i class="fa fa-info-circle"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
                             </div>
-                        </form>
+                        </div>
+                        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                            
+                        </div>
                     </div>
                 </div>
-               <div class="card-body">
-                <table id="datatableBasic" class="table table-borderless table-striped"
-                    style="border-collapse: collapse; border-radius: 4px; overflow: hidden;">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th class="text-center">#</th>
-                            <th>เจ้าหน้าที่</th>
-                            <th>ตำแหน่ง</th>
-                            <th>Book Bank</th>
-                            <th class="text-right">เงินเดือน</th>
-                            <th class="text-center">ปี</th>
-                            <th class="text-center">เดือน</th>
-                            <th class="text-center"><i class="fa fa-bars"></i></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php $i = 0; @endphp
-                        @foreach ($result as $res)
-                        @php $i++; @endphp
-                        <tr>
-                            <td class="text-center">{{ $i }}</td>
-                            <td>{{ $res->name }}</td>
-                            <td>{{ $res->position }}</td>
-                            <td>{{ $res->acc_no }}</td>
-                            <td class="text-right">{{ number_format($res->salary,2) }} ฿</td>
-                            <td class="text-center">{{ $res->year }}</td>
-                            <td class="text-center">{{ MonthThai(date('Y-'.$res->month.'-d')) }}</td>
-                            <td class="text-center">
-                                <a href="#" class="btn btn-outline-info btn-sm btn-block">
-                                    <i class="fa fa-info-circle"></i>
-                                </a>
-                            </td>
-                        </tr>
+            </div>
+        </div>
+    </div>
+    @include('layouts.footers.auth')
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="import" tabindex="-1" aria-labelledby="importModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="importModalLabel"><i class="fa fa-cloud-upload-alt"></i> นำเข้าข้อมูลเงินเดือน</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="frmImport" method="POST" enctype="multipart/form-data" action="{{ url('finance/import') }}">
+                    {{ csrf_field() }}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="custom-file">
+                                <input id="select-file" name="select-file" type="file" class="custom-file-input" required>
+                                <label class="custom-file-label" for="select-file">เลือกไฟล์ที่จะอัพโหลด </label>
+                                <small class="text-danger">(ไฟล์นามสกุล .xlsx หรือ .xls เท่านั้น สามารถดูได้จากคู่มือการอัพโหลด)</small>
+                            </div>
+                            <div class="text-right">
+                                <button type="submit" class="btn btn-success"><i class="fa fa-check-circle"></i> อัพโหลดไฟล์</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                @if (count($salog) > 0)
+                <div class="" style="margin-top: 1rem;">
+                    <small>ประวัติการอัพโหลด</small>
+                    <ul>
+                        @foreach ($salog as $sals)
+                        <li>
+                            <small class="text-muted">{{ $sals->sal_file }} / {{ $sals->create_at }}</small>
+                        </li>
                         @endforeach
-                    </tbody>
-                </table>
+                    </ul>
+                </div>
+                @endif
             </div>
         </div>
     </div>
 </div>
-@include('layouts.footers.auth')
 @endsection
 @section('script')
 <script>
