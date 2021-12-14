13682a0942d53d901b755f485fb931df51f0ad13
lamsri1992 <satiya3577@gmail.com>
2021-12-14 14:11:03 +0700

    add row income print slip

--- resources/views/finance/sal_print.blade.php
+++ resources/views/finance/sal_print.blade.php
@@ -91,6 +91,12 @@
                                                 {{ number_format($income,2) }}
                                             </td>
                                         </tr>
+                                        <tr style="font-weight: bold;">
+                                            <td>รายรับสุทธิ</td>
+                                            <td colspan="3">
+                                                {{ number_format($total,2) }}
+                                            </td>
+                                        </tr>
                                     </table>
                                 </div>
                                 <div class="col-md-6" style="font-size: 14px;">
@@ -150,7 +156,7 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        // window.print();
+        window.print();
     </script>
 </body>
 
