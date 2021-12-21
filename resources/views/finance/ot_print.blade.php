ca2e7663748036e9eb7576926878aebc666e6451
lamsri1992 <satiya3577@gmail.com>
2021-12-21 13:55:59 +0700

    Update Slip OT Print

--- resources/views/finance/ot_print.blade.php
+++ resources/views/finance/ot_print.blade.php
@@ -110,7 +110,7 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        // window.print();
+        window.print();
     </script>
 </body>
 
